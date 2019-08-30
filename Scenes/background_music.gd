98678dc9a41285c59cfe52228a7ea76e00e78c79
Virento Voshe <30956131+userinfamous@users.noreply.github.com>
2019-08-31 00:07:26 +0700

    restructuring

--- Scenes/background_music.gd
+++
@@ -1,6 +0,0 @@
-extends AudioStreamPlayer2D
-
-onready var music = get_node("background_music")
-
-func _process(delta):
-	music.play()
---
+++ game/Start_Menu.gd
@@ -0,0 +1,16 @@
+#Start_Menu.gd
+extends Control
+
+func _on_Start_Button_pressed():
+	get_tree().change_scene("res://game/Level1.tscn")
+	pass # replace with function body
+
+
+func _on_Exit_Game_pressed():
+	get_tree().quit()
+	pass # replace with function body
+
+
+func _on_Options_pressed():
+	print("Nothing Yet!")
+	pass # replace with function body
